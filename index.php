484936ebffd9aecfb7fc8398b5d44e7e5d34053b
g-alfieri <g-alfieri@users.noreply.github.com>
2015-04-20 23:24:05 +0200

    primo punto lista

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
-<?php
-
-echo("Hello World!! Edited with notepad++!, commit by notepad svn plugin");
-
-echo("This second line is very cool!");
-
-?>
+<ol>
+<div>
+<li>Installare Tortoise SVN 64-bit
+<a href="https://sourceforge.net/projects/tortoisesvn/files/1.8.11/Application/TortoiseSVN-1.8.11.26392-x64-svn-1.8.13.msi/download?accel_key=61%3A1429564046%3Ahttp%253A//tortoisesvn.net/downloads.html%3Ab9f46236%2461e5bc69b034669d162d96bdd6d529e961bfbb96&click_id=3f57cf5c-e7a1-11e4-bb9b-0200ac1d1d8a-1&source=accel">Tortoise</a>
+</li>
+</div>
